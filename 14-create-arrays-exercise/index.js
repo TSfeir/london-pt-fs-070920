f11f9ad2d99cdfdba9d7ee32d355ca742d3ed41e
Vasile Cojusco <codersinhoods@MacBook-Pro-2.broadband>
2020-09-21 07:46:13 +0100

    w3 exercises

---
+++ 14-create-arrays-exercise/index.js
@@ -0,0 +1,83 @@
+/**
+ * Exercise 1
+ *
+ * Create an array {wildAnimals} and add 'tiger', 'rabbit',
+ * 'panda' and 'giraffe' into it as strings
+ */
+
+
+/**
+ * Exercise 2
+ *
+ * Create an array {pets} and add 'cat', 'dog' and 'hamster'
+ * into it as strings
+ */
+
+/**
+ * Exercise 3
+ *
+ * Create a variable {animals}. It should include all {wildAnimals}
+ * and all {pets}. Find the way to concatenate your arrays with JS
+ */
+
+/**
+ * Exercise 4
+ *
+ * create a {dogIndex} variable and by using findIndex JS method
+ * find dog index in "animals" array
+ */
+
+/**
+ * Exercise 5
+ *
+ * create a {pandaIndex} variable and by using indexOf JS method
+ * find panda index in "animals" array
+ */
+
+/**
+ * create a variable with name {numbers}  which will be an array
+ * and contain numbers from 1 to 9(ascending) and from 9 to 1(descending)
+ * (each number should be twice in this array).
+ */
+
+/**
+ * Exercise 6
+ *
+ * Create variable {first4DigitIndex} and from {numbers},
+ * find index of first instance of digit 4
+ */
+
+/**
+ * Exercise 7
+ *
+ * Create variable {ast4DigitIndex} and from {numbers}
+ * find index of last instance of digit 4
+ */
+
+/**
+ * Exercise 8
+ *
+ * Create variable {hasSeven} check does {numbers}
+ * contains 7
+ */
+
+/**
+ * Exercise 9
+ *
+ * Create variable {hasTwenty} check does {numbers}
+ * contains 20
+ */
+
+/**
+ * Exercise 10
+ *
+ * Create variable {moreThanFour} returns first number
+ * from {numbers} that is more than 4
+ */
+
+/**
+ * Exercise 11
+ *
+ * Create variable {moreEqualThanSeven} returns first number
+ * from {numbers} that are more or equal 7
+ */
---
+++ 25-DIY-loops-js-exercise/index.js
@@ -0,0 +1,90 @@
+const dataTypes = [
+  4,
+  "hello",
+  "2",
+  { name: "Alex" },
+  54,
+  "random",
+  23,
+  "kitten",
+  13.21,
+  [1, 2, 3, 4],
+  { type: "boolean" },
+  [32, 52],
+  43.1,
+];
+
+/**
+ * Exercise 1
+ *
+ * create a function {sortedData} which will take data as param
+ * (you can use){dataTypes} and sort it by type:
+ * 1. strings,
+ * 2. integers,
+ * 3. floats,
+ * 4. arrays,
+ * 5. objects
+ *
+ * and return object with property names equals types, and values should be
+ * arrays with  corespondent data
+ *
+ * Ex: const data = [5, 'kitten', 3.12, 12]
+ *
+ * result:
+ * {
+ *  integers: [5, 12],
+ *  strings: ['kitten'],
+ *  floats: [3.12]
+ * }
+ */
+
+
+/**
+ * Exercise 2
+ *
+ * create a function {multipliedByNextNumber} which takes
+ * an array of numbers and returns a new array where number equals
+ * multiplication of number from passed array and the following number.
+ * Presume that the following number for the last one is 1.
+ *
+ * Ex: pass [3,2,6]
+ * result: [6, 12, 6]
+ */
+
+/**
+ * Exercise 3
+ *
+ * create a function {multipliedEvenNumbers} which multiply
+ * even number from passed array with following even number.
+ * If there is no following even numbers, multiply by 2.
+ *
+ * return new numbers as new array
+ *
+ * ex: multipliedEvenNumbers([4,3,6,8,5,7])
+ * result: [24, 3, 48, 16, 5, 7]
+ */
+
+
+/**
+ * Exercise 4
+ *
+ * create a function {multipliedOddNumbers} which multiply
+ * even number from passed array with following even number.
+ * If there is no following even numbers, multiply by 1.
+ *
+ * return new numbers as new array
+ *
+ * ex: multipliedEvenNumbers([4,3,6,8,5,7])
+ * result: [4, 15, 6, 8, 35, 7]
+ */
+
+/**
+ * Exercise 5
+ *
+ * create a function {multipliedEvenOddNumbers} which will take
+ * an array and type "even" or "odd".
+ *
+ * If you pass "even" do the same what you have for exercise 3
+ * if you pass "odd" do the same what you have for exercise 4
+ * else return original array.
+ */
