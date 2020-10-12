76afdd719f63728f4433fc409945b20e74722298
Vasile Cojusco <codersinhoods@Vasiles-MBP.broadband>
2020-10-12 20:28:56 +0100

    M2W3

---
+++ 45-js-pinterest-simple/tests/tests.js
@@ -0,0 +1,12 @@
+const {
+	fireEvent
+} = require("@testing-library/dom/dist/@testing-library/dom.umd.js");
+
+const {
+	fireEvent
+} = require("@testing-library/dom/dist/@testing-library/dom.umd.js");
+
+// Exercise #
+describe("", () => {
+	test("", () => {});
+});
